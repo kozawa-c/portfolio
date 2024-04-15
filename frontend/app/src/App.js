85683c2e0c3a2532a5699fc3ee8eff255d681ef1
sjc-kozawa <kozawa.chikako@sjc-inc.co.jp>
2024-04-15 17:50:22 +0900

    dockerの設定見直し

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -5,6 +5,16 @@ import { FaAppleWhole } from "react-icons/fa6";
 function App() {
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a>
+          </li>
+          <li>
+            <a href="https://react-icons.github.io/react-icons/" target="_blank" rel="noopener noreferrer">React Icons</a>
+          </li>
+        </ul>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
